b320b7b9324e5a958aa32a37b236debb2eb1a80b
Nagesh Mali <malinagesh60@gmail.com>
2022-12-23 16:40:42 +0530

    done with progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import { Layout } from '../components';
 import {StateContext} from '../context/StateContext';
 import {Toaster} from 'react-hot-toast';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
         <Toaster/>
+        <NextNProgress color="#f02d34"/>
         <Component {...pageProps} />
       </Layout>
     </StateContext>
